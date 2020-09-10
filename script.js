70cd57839380d9e54219ad74723f318aedb7d266
TG <43038974+tamaghan@users.noreply.github.com>
2020-09-10 00:03:02 -0400

    Changed navigation bar

---
+++ script.js
@@ -0,0 +1,51 @@
+function go_dark() {
+    // var back_sec = document.querySelectorAll(".sec:nth-child(2n)");
+    // var back1 = document.getElementById('real-dashboard');
+    // var head = document.getElementById('dash-head');
+    // document.getElementById('semester').style.color = "white";
+    // document.getElementById('prog_bar').style.backgroundColor = "#242424";
+    // var cards_mob = document.querySelectorAll(".dash-card-mobile");
+    // var cards = document.querySelectorAll(".dash-card");
+    // for (let i = 0; i < back_sec.length; i++) {
+    //     back_sec[i].style.backgroundColor = "black";
+    //     // cards[i].style.boxShadow = '-5px 10px 10px 0 #000000';
+    // }
+    // console.log("tg");
+    // for (let i = 0; i < cards.length; i++) {
+    //     cards_mob[i].style.boxShadow = '-5px 10px 10px 0 #000000';
+    //     cards[i].style.boxShadow = '-5px 10px 10px 0 #000000';
+    // }
+    // var elements = document.getElementsByClassName('dash-teams'); // get all elements
+	// for(var i = 0; i < elements.length; i++){
+	// 	elements[i].style.backgroundColor = "#242424";
+	// 	elements[i].style.color = "white";
+	// 	elements[i].style.boxShadow = '-5px 10px 10px 0 #000000';
+	// }
+    // back1.style.backgroundColor = 'rgb(22, 22, 22)';
+    // head.style.backgroundImage = 'linear-gradient(50deg, #fbb040 0%, #f9ed32 74%)';
+
+}
+
+function go_light() {
+
+    // var back = document.getElementById('real-dashboard');
+    // var head = document.getElementById('dash-head');
+    // document.getElementById('semester').style.color = "black";
+    // document.getElementById('prog_bar').style.backgroundColor = "#dedede";
+    //
+    // var cards_mob = document.querySelectorAll(".dash-card-mobile");
+    // var cards = document.querySelectorAll(".dash-card");
+    // for (let i = 0; i < cards.length; i++) {
+    //     cards_mob[i].style.boxShadow = '-5px 10px 10px 0 #bababa';
+    //     cards[i].style.boxShadow = '-5px 10px 10px 0 #bababa';
+    // }
+    // var elements = document.getElementsByClassName('dash-teams'); // get all elements
+	// for(var i = 0; i < elements.length; i++){
+	// 	elements[i].style.backgroundColor = "#dedede";
+	// 	elements[i].style.color = "black";
+	// 	elements[i].style.boxShadow = '-5px 10px 10px 0 #bababa';
+	// }
+    // back.style.backgroundColor = 'white';
+    // head.style.backgroundImage = 'linear-gradient(50deg, #65799b 0%, #5e2563 74%)';
+
+}
